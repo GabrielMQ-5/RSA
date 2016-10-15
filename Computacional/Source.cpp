71b43963b3877ee250028d6d791f9e16e0b45a7a
GianfrancoMS <gianfrancomonsal@gmail.com>
2016-10-14 22:07:25 -0500

    Modular Exponentiaton

--- Computacional/Source.cpp
+++ Computacional/Source.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
-#include "Numbers.h"
 
 int main(array<System::String ^> ^args)
 {
 	//Work to be done here
-	std::cout << Numbers::ModularExponentiation(2, 123, 2) << std::endl;
-	system("pause");
 	return 0;
 }
